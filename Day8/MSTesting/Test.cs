9b1ea46c0584cd95bfaa8f87afb091c24183fa69
VISHAL SAMPETA <102171620+VISHU-24@users.noreply.github.com>
2025-02-07 09:08:47 +0530

    Add files via upload

---
+++ Day8/MSTesting/Test.cs
@@ -0,0 +1,46 @@
+﻿using AssignmentQ1;
+namespace Test
+{
+    [TestClass]
+    public sealed class Test
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Calculator cal = new Calculator();
+            double res = cal.Add(20, 3);
+            Assert.AreEqual(res, 23);
+        }
+        [TestMethod]
+        public void Sub()
+        {
+            Calculator cal = new Calculator();
+            double res = cal.Subtract(20, 30);
+            Assert.AreEqual(res, -10);
+        }
+        [TestMethod]
+        public void Mul()
+        {
+            Calculator cal = new Calculator();
+            double res = cal.Multiply(10, 4);
+            Assert.AreEqual(res, 40);
+        }
+        [TestMethod]
+        public void Div()
+        {
+
+            Calculator cal = new Calculator();
+            double res = cal.Divide(30, 3);
+            Assert.AreEqual(10, res);
+
+        }
+        [TestMethod]
+        public void DivbyZero()
+        {
+            Calculator cal = new Calculator();
+            Assert.ThrowsException<DivideByZeroException>(() => cal.Divide(10, 0));
+        }
+
+    }
+
+}
